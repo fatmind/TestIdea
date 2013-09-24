9eb3fdd10bffbbfbc411c9ccb946ddfcb0662ae5
fatmind <m.shijianwu@gmail.com>
2013-09-24 19:36:06 +0800

    dp temp commit

---
+++ src/main/java/math/dp/MinCion.java
@@ -0,0 +1,18 @@
+package math.dp;
+
+/**
+ * 尝试dp简单问题：如何用最少的硬币，凑够某个金额
+ * @author bohan.sj
+ */
+public class MinCion {
+	
+	
+	
+	public int dpEval(int sumValue) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
